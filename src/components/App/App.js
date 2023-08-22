0f476c92cc47509abcde3abaa8975e8f4b91aeee
Agurchkova <copyart@mail.ru>
2023-08-22 16:03:22 +0300

    content

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import { Route, Routes } from "react-router-dom";
+
+import Main from "../Main/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,18 @@
+import "./Main.css";
+import Header from "../Header/Header";
+import Content from "../Content/Content";
+// import Footer from "../Footer/Footer";
+
+function Main() {
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <Content />
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default Main;
