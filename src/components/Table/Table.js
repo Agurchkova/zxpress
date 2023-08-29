2f283de8dbd525a3776b05c59b5a2bbb6221e1e8
Agurchkova <copyart@mail.ru>
2023-08-29 21:06:47 +0300

    adaptive

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -6,20 +6,34 @@ import { Link } from "react-router-dom";
 function Table() {
   return (
     <>
-      <div className="content__table">
+      {/* <div className="content__table">
         <div className="content__table-header">
           <p className="content__table-name">Название</p>
           <p className="content__table-name">Город</p>
           <p className="content__table-name">Форма</p>
           <p className="content__table-name">Года издания</p>
         </div>
-        <TableCatalog />
-        <div className="content__dash-long">
-          <img className="content__dash-image" src={dashLong} alt="dash" />
-        </div>
+    <TableCatalog />
+      <div className="content__dash-long">
+        <img className="content__dash-image" src={dashLong} alt="dash" />
+      </div>
+    </div>  */}
+
+      <table className="content__table">
+        <thead className="content__table-header">
+          <tr>
+            <th className="content__table-name">Название</th>
+            <th className="content__table-name">Город</th>
+            <th className="content__table-name">Форма</th>
+            <th className="content__table-name">Года издания</th>
+          </tr>
+        </thead>
+      </table>
+      {/* <TableCatalog /> */}
+      <div className="content__dash-long">
+        <img className="content__dash-image" src={dashLong} alt="dash" />
       </div>
       <Link className="content__table-letter">A</Link>
-      <TableCatalog />
     </>
   );
 }
